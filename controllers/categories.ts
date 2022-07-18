a20c4ed6f8141238225c952b52be0a497d9e5b85
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-07-18 18:40:57 +0200

    created routes for category admin

--- controllers/categories.ts
+++ controllers/categories.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
+import ICategory from '../interfaces/ICategory';
 import Category from '../models/category';
+import { ErrorHandler } from '../helpers/errors';
 
 const getAllCategories = (async (
   req: Request,
@@ -21,4 +23,63 @@ const getAllCategories = (async (
   }
 }) as RequestHandler;
 
-export default { getAllCategories };
+// GET category by id
+const getOneCategory = (async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { idCategory } = req.params;
+    const category = await Category.getCategoryById(Number(idCategory));
+    category ? res.status(200).json(category) : res.sendStatus(404);
+  } catch (err) {
+    next(err);
+  }
+}) as RequestHandler;
+
+// POST category
+const addCategory = (async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const category = req.body as ICategory;
+    category.id = await Category.addCategory(category);
+    res.status(201).json(category);
+  } catch(err) {
+    console.log(err)
+    next(err);
+  }
+}) as RequestHandler;
+
+// PUT category
+const updateCategory = (async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { idCategory } = req.params;
+    const categoryUpdated = await Category.updateCategory(Number(idCategory), req.body as ICategory); //categoryUpdated => boolean
+    if (categoryUpdated) {
+      const category = await Category.getCategoryById(Number(idCategory));
+      res.status(200).send(category); // react-admin needs this response
+    } else {
+        throw new ErrorHandler(500, 'Category cannot be updated');
+    }
+  } catch(err) {
+    next(err);
+  }
+}) as RequestHandler;
+
+// DELETE category
+const deleteCategory = (async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { idCategory } = req.params;
+    const category = await Category.getCategoryById(Number(idCategory));
+    const categoryDeleted = await Category.deleteCategory(Number(idCategory)); //categoryDelected => boolean
+    if(categoryDeleted) {
+      res.status(200).send(category); //needed by react-admin
+    } else {
+      throw new ErrorHandler(500, 'Category cannot be deleted');
+    }
+  } catch(err) {
+    next(err);
+  }
+}) as RequestHandler;
+
+export default { getAllCategories, getOneCategory, addCategory, updateCategory, deleteCategory };
--- models/category.ts
+++ models/category.ts
@@ -1,12 +1,24 @@
-import ICategory from '../interfaces/IArticle';
+import ICategory from '../interfaces/ICategory';
 import connection from '../db-config';
 import IArticleCategory from '../interfaces/IArticleCategory';
+import { ResultSetHeader } from 'mysql2';
 
 // GET all categories
 const getAllCategories = async () : Promise<IArticleCategory[]> => {
   const results = await connection
   .promise()
   .query<IArticleCategory[]>('SELECT * FROM categories');
+  return results[0];
+};
+
+// GET category by id
+const getCategoryById = async (idCategory: number): Promise<ICategory> => {
+  const [results] = await connection
+    .promise()
+    .query<ICategory[]>(
+      'SELECT * FROM categories WHERE id = ?',
+      [idCategory]
+    );
   return results[0];
 };
 
@@ -37,4 +49,47 @@ const getCategoriesByArticle = async (
   return results[0];
 };
 
-export default { getAllCategories, getCategoriesByPackage, getCategoriesByArticle };
+// POST category
+const addCategory = async (category: ICategory): Promise<number> => {
+  const results = await connection
+    .promise()
+    .query<ResultSetHeader>(
+      'INSERT INTO categories (name, description) VALUES (?,?)',
+      [category.name, category.description]
+    );
+  return results[0].insertId;
+};
+
+// PUT category
+const updateCategory = async (idCategory: number, category: ICategory): Promise<boolean> => {
+    let sql = 'UPDATE categories SET ';
+    const sqlValues: Array<string | number> = [];
+    let oneValue = false;
+    if (category.name) {
+      sql += 'name = ?';
+      sqlValues.push(category.name);
+      oneValue = true;
+    }
+    if (category.description) {
+      sql += oneValue ? ' , description = ? ' : ' description = ? ';
+      sqlValues.push(category.description);
+      oneValue = true;
+    }
+    sql += ' WHERE id = ?';
+    sqlValues.push(idCategory);
+  
+    const results = await connection
+      .promise()
+      .query<ResultSetHeader>(sql, sqlValues);
+    return results[0].affectedRows === 1;
+};
+
+// DELETE category
+const deleteCategory = async (idCategory: number): Promise<boolean> => {
+    const results = await connection
+      .promise()
+      .query<ResultSetHeader>('DELETE FROM categories WHERE id = ?', [idCategory]);
+    return results[0].affectedRows === 1;
+  };
+
+export default { getAllCategories, getCategoryById, getCategoriesByPackage, getCategoriesByArticle, addCategory, updateCategory, deleteCategory };
